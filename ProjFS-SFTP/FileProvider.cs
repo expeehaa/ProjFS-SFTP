d5d115dfc96016f8b21003fda3bd355731b2e9ed
expeehaa <expeehaa@outlook.com>
2019-06-08 00:50:38 +0200

    Add class FileProvider with constructor.

---
+++ ProjFS-SFTP/FileProvider.cs
@@ -0,0 +1,13 @@
+﻿using Renci.SshNet;
+
+namespace ProjFS_SFTP {
+	public class FileProvider {
+		private readonly ConnectionInfo connectionInfo;
+		private readonly string virtualizationPath;
+
+		public FileProvider(ConnectionInfo conInfo, string virtualizationPath) {
+			connectionInfo = conInfo;
+			this.virtualizationPath = virtualizationPath;
+		}
+	}
+}
